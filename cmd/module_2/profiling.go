417cc4ca60923cd8cfe2c8a86563aae5c4d715c2
CCat <ccathlete01@gmail.com>
2024-12-09 07:13:17 +0100

    Lecture - Types of profiling.

--- cmd/module_2/profiling.go
+++ cmd/module_2/profiling.go
@@ -7,14 +7,20 @@ import (
 	"sync"
 )
 
-// Profiling the booting of a local server.
-// To view the results graph run go tool pprof (it's a cli)
-// You need to create a server like I did here in Ex6
-// and get the profile data archive by a get request(or going to the url
-// with the browser) to: localhost:3030/debug/pprof
-// port defined by you.
-// You then cd to the location of the prof archive and
-// run "go tool pprof".
+/*
+Profiling the booting of a local server.
+To view all results go to: localhost:3030/debug/pprof
+To view the results graph run go tool pprof (it's a cli)
+You need to create a server like I did here in Ex6
+and get the profile data archive by a get request(or going to the url
+with the browser) to: localhost:3030/debug/pprof/profile
+port defined by you.
+You then cd to the location of the prof archive and
+run "go tool pprof".
+
+To dowlnoad heap profiling results archive go to
+localhost:3030/debug/pprof/heap
+*/
 func Ex6() {
 	var wg sync.WaitGroup
 	wg.Add(1)
