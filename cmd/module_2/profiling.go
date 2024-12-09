ad4591bcd2368f6d50c08ddd045fb85197db51f8
CCat <ccathlete01@gmail.com>
2024-12-09 05:59:33 +0100

    Lecture - Types of profiling.

--- cmd/module_2/profiling.go
+++ cmd/module_2/profiling.go
@@ -9,6 +9,12 @@ import (
 
 // Profiling the booting of a local server.
 // To view the results graph run go tool pprof (it's a cli)
+// You need to create a server like I did here in Ex6
+// and get the profile data archive by a get request(or going to the url
+// with the browser) to: localhost:3030/debug/pprof
+// port defined by you.
+// You then cd to the location of the prof archive and
+// run "go tool pprof".
 func Ex6() {
 	var wg sync.WaitGroup
 	wg.Add(1)
