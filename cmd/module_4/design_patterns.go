87e40017c4fe78766507845d2d7c5704a6091dd3
CCat <ccathlete01@gmail.com>
2024-12-10 07:38:46 +0100

    Lecture - Common design patterns 1.

--- cmd/module_4/design_patterns.go
+++ cmd/module_4/design_patterns.go
@@ -13,6 +13,9 @@ type mySingleton struct {
 var once sync.Once
 var instance *mySingleton
 
+// The once variable will make sure that the memory allocation
+// would happen only once. The rest of the times the Get function
+// is called, the same instance (with the same address) is returned.
 func GetInstance() *mySingleton {
 	once.Do(func() {
 		fmt.Println("Allocating a new address for the instance.")
